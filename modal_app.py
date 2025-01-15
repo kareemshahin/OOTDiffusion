dd60cdc6da601306749b4c0695128ae67e6180a9
Kareem Shahin <kareem@Mac.lan>
2025-01-14 21:09:01 -0800

    use mounted inputs

--- modal_app.py
+++ modal_app.py
@@ -11,20 +11,6 @@ dockerfile_image = (
     .pip_install_from_requirements("./requirements.txt")
     .pip_install("huggingface_hub==0.25.2")
     .add_local_dir(".", remote_path="/app")
-    #.pip_install("numpy==1.24.4")
-    #.pip_install("scipy==1.10.1")
-    #.pip_install("scikit-image==0.21.0")
-    #.pip_install("opencv-python==4.7.0.72")
-    #.pip_install("pillow==9.4.0")
-    #.pip_install("diffusers==0.24.0")
-    #.pip_install("transformers==4.36.2")
-    #.pip_install("accelerate==0.26.1")
-    #.pip_install("matplotlib==3.7.4")
-    #.pip_install("tqdm==4.64.1")
-    #.pip_install("gradio==4.16.0")
-    #.pip_install("config==0.5.1")
-    #.pip_install("einops==0.7.0")
-    #.pip_install("onnxruntime==1.16.2")
 )
 checkpoints = modal.Volume.from_name("ootd-checkpoints")
 viton_output = modal.Volume.from_name("viton_output")
@@ -33,31 +19,52 @@ input_images = modal.Volume.from_name("input_images")
 volume_map = {
     "/app/checkpoints": checkpoints,
     "/app/run/outputs": viton_output,
-    "/app/run/inputs": input_images,
+    "/app/run/input_images": input_images,
 }
 WORKING_DIR = "/app/run"
 
 @app.function(gpu="A100", image=dockerfile_image, volumes=volume_map)
-def test():
+def try_on(
+    model_path="/app/run/examples/model/01008_00.jpg",
+    cloth_path="/app/run/examples/garment/00055_00.jpg",
+    category="full", scale="2.0", sample="1"
+):
     import subprocess
+
+    model_type = 'dc'
+    input_category = '2'
+
+    if category in ['lower', 'upper']:
+        model_type = 'hd'
+        input_category = '0' if category == 'upper' else '1'
 
     cmdz = [
         "python",
         "run_ootd.py",
         "--model_path",
-        "/app/run/examples/model/01008_00.jpg",
+        model_path,
         "--cloth_path",
-        "/app/run/examples/garment/00055_00.jpg",
+        cloth_path,
+        "--category",
+        input_category,
+        "--model_type",
+        model_type,
         "--scale",
-        "2.0",
+        scale,
         "--sample",
-        "4"
+        sample,
     ]
+    print(*cmdz)
 
     retcode = subprocess.run(cmdz, cwd=WORKING_DIR)
     print(retcode)
+    #print(f"model_path={model_path},cloth_path={cloth_path},category={category}({model_type},{input_category})")
 
 
-@app.local_entrypoint()
-def main():
-    test.remote()
+#@app.local_entrypoint()
+#def main(
+#    model_path="/app/run/examples/model/01008_00.jpg",
+#    cloth_path="/app/run/examples/garment/00055_00.jpg",
+#    category="full", scale="2.0", sample="1"
+#):
+#    try_on.remote(model_path, cloth_path, category, scale, sample)
